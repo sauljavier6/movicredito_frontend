0e327f0489ed470cc16f9afd2963f27c644ec574
sauljavier <psauljavier6@gmail.com>
2025-10-06 00:57:33 -0700

    correccion de rutas

--- src/components/layouts/AppLayout.tsx
+++ src/components/layouts/AppLayout.tsx
@@ -1,8 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { useState } from "react";
 import { FaUserCircle } from "react-icons/fa";
-import { Link } from "react-router-dom";
-
 
 
 const AppLayout = () => {
@@ -31,16 +29,17 @@ const AppLayout = () => {
 
             {/* Navegación */}
             <div className="flex gap-6 text-white font-medium items-center">
-              <Link to="/" className="hover:text-green-400 transition">Inicio</Link>
-              <Link to="/admin/clientes" className="hover:text-green-400 transition">Clientes</Link>
-              <Link to="/admin/catalogo" className="hover:text-green-400 transition">Catalogo</Link>
-              <Link to="/admin/creditos" className="hover:text-green-400 transition">Créditos</Link>
-              <Link to="/admin/reportes" className="hover:text-green-400 transition">Reportes</Link>
+              <a href="/admin" className="hover:text-green-400 transition">Inicio</a>
+              <a href="/admin/clientes" className="hover:text-green-400 transition">Clientes</a>
+              <a href="/admin/catalogo" className="hover:text-green-400 transition">Catalogo</a>
+              <a href="/admin/creditos" className="hover:text-green-400 transition">Créditos</a>
+              <a href="/admin/reportes" className="hover:text-green-400 transition">Reportes</a>
 
+              {/* Icono o foto de usuario */}
               {!isLoggedIn ? (
-                <Link to="/login" className="text-3xl hover:text-green-400 transition">
+                <a href="/login" className="text-3xl hover:text-green-400 transition">
                   <FaUserCircle />
-                </Link>
+                </a>
               ) : (
                 <img
                   src={profileImage}
