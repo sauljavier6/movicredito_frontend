9609e13aa33a37279b49f6802832aff094234585
Saul Javier Peralta Gomez <125310857+sauljavier6@users.noreply.github.com>
2026-09-12 12:39:49 -0700

    refactor: keep admin registration closed

--- src/pages/auth/AuthPage.tsx
+++ src/pages/auth/AuthPage.tsx
@@ -1,20 +1,5 @@
-
-import { useState } from "react";
 import LoginComponent from "../../components/auth/LoginComponent";
-import RegisterComponent from "../../components/auth/RegisterComponent";
 
-const AuthPage = () => {
-  const [isRegistering, setIsRegistering] = useState(false);
-
-  return (
-    <div>
-    {isRegistering ? (
-    <RegisterComponent onBack={() => setIsRegistering(false)} />
-    ) : (
-    <LoginComponent onRegister={() => setIsRegistering(true)} />
-    )}
-    </div>
-  );
-};
+const AuthPage = () => <LoginComponent />;
 
 export default AuthPage;
