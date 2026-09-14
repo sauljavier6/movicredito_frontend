8b29ba6f3e12a2c5e4ce289625c859410d24e50d
Saul Javier Peralta Gomez <125310857+sauljavier6@users.noreply.github.com>
2026-09-13 19:04:43 -0700

    feat: install global auth fetch interceptor

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import './index.css'
 import App from './App.tsx'
 import { ToastContainer } from 'react-toastify';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { installAuthFetchInterceptor } from './utils/session';
 
 const queryClient = new QueryClient();
 
+installAuthFetchInterceptor();
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
